cb77ff5f3dfa4a171c8dd1fe1758b95238734e65
Malte Jesgarzewsky <maljes@users.noreply.github.com>
2016-11-24 19:40:58 +0100

    Update mittelwertberechnung_2016-11-21.cpp

--- mittelwertberechnung_2016-11-21.cpp
+++ mittelwertberechnung_2016-11-21.cpp
@@ -10,31 +10,34 @@ using namespace std;
 int main()
 {
 	char antwort;
-	float eingabe, zahl, tmp = 0;
-	int i=1;
+	float eingabe, zahl, tmp = 0, max = 0, min = 0;
+	int i = 1;
 
+	cout << "Gib eine Zahl ein: "; 
+	cin >> eingabe; /* Eingabe der ersten Zahl */
+	cout << "Berechnung des Mittelwertes:" << endl;
 
-	cout << "Gib eine Zahl ein:" << endl;
-	cin >> eingabe;
-	
 	do {
-		tmp = eingabe + tmp;
-		zahl = tmp / i;
-		cout << i << ". Zahl: " << eingabe << endl;
-		cout << "Mittelwert: " << zahl << endl;
-		cout << "Soll eine weitere Zahl eingegeben werden (J/N)?" << endl;
+		tmp = eingabe + tmp; /* Addieren der Eingabe sowie des Wertes der Vorrunde */
+		zahl = tmp / i; /* Teilen des Ergebnisses durch die Anzahl der Runden */
+		cout << i << ". Zahl: " << eingabe << endl; /* Ausgabe der Runde sowie der eingegebenen Zahl */
+		cout << "Mittelwert = " << zahl << endl; /* Ausgabe des errechneten Mittelwertes */
+		if (zahl > max) max = zahl; /* Überprüfung ob Ergebnis am größten */
+		if ((min == 0 && zahl < max) || zahl < min) min = zahl; /* Überprüfung ob Ergebnis am kleinsten sowie Überprüfung ob min = 0 */
+		cout << "Soll eine weitere Zahl eingegeben werden (J/N)? ";
 		cin >> antwort;
-		antwort = char(toupper(antwort));
-		if (antwort == 'J') {
-			cout << "Geben Sie die naechste Zahl ein:" << endl;
+		antwort = char(toupper(antwort)); /* kleine Buchstaben in große umwandeln */
+		if (antwort == 'J') { /* Überprüfung ob Eingabe = J */
+			cout << "Geben Sie die naechste Zahl ein: ";
 			cin >> eingabe;
-			i++;
+			i++; /* Runde + 1 */
 		}
-		if (antwort == 'N') {
+		if (antwort == 'N') { /* Überprüfung ob Eingabe = N */
 			cout << "Der endgueltige Mittelwert lautet: " << zahl << endl;
+			cout << "Der groesste Mittelwert lautet: " << max << endl;
+			cout << "Der kleinste Mittelwert lautet: " << min << endl;
 		}
 	} while (antwort == 'J' && antwort != 'N');
 
-    return 0;
+	return 0;
 }
-
