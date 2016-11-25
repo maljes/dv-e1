80390f3ee86ac9acac6a82988a9fe6e337c12375
Malte Jesgarzewsky <maljes@users.noreply.github.com>
2016-11-25 09:57:46 +0100

    Update mittelwertberechnung_2016-11-21.cpp

--- mittelwertberechnung_2016-11-21.cpp
+++ mittelwertberechnung_2016-11-21.cpp
@@ -13,35 +13,35 @@ int main()
 	float eingabe, zahl, tmp = 0, max = 0, min = 0;
 	int i = 1;
 
-	cout << "Gib eine Zahl ein: "; 
-	cin >> eingabe; /* Eingabe der ersten Zahl */
+	cout << "Gib eine Zahl ein: ";
+	cin >> eingabe;
 	cout << "Berechnung des Mittelwertes:" << endl;
 
 	do {
-		tmp = eingabe + tmp; /* Addieren der Eingabe sowie des Wertes der Vorrunde */
-		zahl = tmp / i; /* Teilen des Ergebnisses durch die Anzahl der Runden */
-		cout << i << ". Zahl: " << eingabe << endl; /* Ausgabe der Runde sowie der eingegebenen Zahl */
-		cout << "Mittelwert = " << zahl << endl; /* Ausgabe des errechneten Mittelwertes */
-		if (zahl > max) max = zahl; /* Überprüfung ob Ergebnis am größten */
-		if ((min == 0 && zahl < max) || zahl < min) min = zahl; /* Überprüfung ob Ergebnis am kleinsten sowie Überprüfung ob min = 0 */
+		tmp = eingabe + tmp;
+		zahl = tmp / i;
+		cout << i << ". Zahl: " << eingabe << endl;
+		cout << "Mittelwert = " << zahl << endl;
+		if (eingabe > max) max = eingabe;
+		if ((min == 0 && eingabe < max) || zahl < min) min = eingabe; 
 		do {
 			cout << "Soll eine weitere Zahl eingegeben werden (J/N)? ";
 			cin >> antwort;
-			antwort = char(toupper(antwort)); /* kleine Buchstaben in große umwandeln */
-			if (antwort == 'J') { /* Überprüfung ob Eingabe = J */
+			antwort = char(toupper(antwort));
+			if (antwort == 'J') {
 				cout << "Geben Sie die naechste Zahl ein: ";
 				cin >> eingabe;
-				i++; /* Runde + 1 */
+				i++;
 			}
-			if (antwort == 'N') { /* Überprüfung ob Eingabe = N */
+			if (antwort == 'N') {
 				cout << "Der endgueltige Mittelwert lautet: " << zahl << endl;
-				cout << "Der groesste Mittelwert lautet: " << max << endl;
-				if (min != 0) /* Überprüfung ob min immer noch 0 */
-					cout << "Der kleinste Mittelwert lautet: " << min << endl;
+				cout << "Der groesste eingegebene Wert lautet: " << max << endl;
+				if (min != 0)
+					cout << "Der kleinste eingegebene Wert lautet: " << min << endl;
 				else
-					cout << "Es wurde kein kleinerer Mittelwert als " << max << " errechnet." << endl;
+					cout << "Es wurde kein kleinerer Wert als " << max << " eingegeben." << endl;
 			}
-		} while (antwort != 'J' && antwort != 'N'); /* Überprüfung ob J oder N gedrückt wurde */
+		} while (antwort != 'J' && antwort != 'N');
 	} while (antwort == 'J' && antwort != 'N');
 
 	return 0;
