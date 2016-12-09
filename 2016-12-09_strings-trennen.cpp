2de57b38ddb9c3dc3dac0a3e55fd91a335daac30
Malte Jesgarzewsky <maljes@users.noreply.github.com>
2016-12-09 12:54:12 +0100

    Update 2016-12-09_strings-trennen.cpp

--- 2016-12-09_strings-trennen.cpp
+++ 2016-12-09_strings-trennen.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 int main()
 {
-	char name[40], vorname[20], nachname[20];
-	int i, o = 0;
+	char name[60], vorname[20], nachname[20], klasse[20];
+	int i, o = 0, t = 0;
 
-	cout << "Geben Sie den Namen ein (Nachname, Vorname): ";
+	cout << "Geben Sie einen Namen ein (Nachname, Vorname, Klasse): ";
 	gets_s(name);
 
-	for (i = 0; i <= 39; i = i++) {
+	for (i = 0; i <= 59; i = i++) {
 		if (name[i] == ',') {
 			nachname[i] = '\0';
 			break;
@@ -22,15 +22,26 @@ int main()
 		nachname[i] = name[i];
 	}
 
-	for (i = i+ 2; i <= 39; i = i++) {
+	for (i = i+2; i <= 59; i = i++) {
+		if (name[i] == ',') {
+			vorname[o] = '\0';
+			break;
+		}
+
 		vorname[o] = name[i];
 		o++;
+	}
+
+	for (i = i+ 2; i <= 59; i = i++) {
+		klasse[t] = name[i];
+		t++;
 		if (name[i] == '\0') {
 			break;
 		}
 	}
 
-	cout << vorname << " " << nachname << endl;
+	cout << vorname << " " << nachname << " (" << klasse << ")" << endl;
 
     return 0;
 }
+
